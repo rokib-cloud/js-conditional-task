2fbe2d34f3c7552c2cd3b06420e41025fc2d3c23
rokib-cloud <rokibh393@gmail.com>
2024-06-30 19:46:31 +0600

    freeDrinks.js

---
+++ freeDrinks.js
@@ -0,0 +1,14 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+const burgerPrice = 420;
+if(burgerPrice > 500){
+    console.log('Free Mojo');
+}
+else{
+    console.log( burgerPrice + 30);
+}
